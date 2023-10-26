a2b84fe7816876fef2d1da29a14ba7a8124cd803
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-26 14:33:36 +0200

    [ init ] php with foreach need fix table

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php 
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css' integrity='sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==' crossorigin='anonymous'/>
+  <title>PHP Hotel</title>
+</head>
+<body>
+<div class="container">
+  <table class="table">
+    <thead>
+      <tr>
+        <?php foreach($hotels[0] as $key => $value): ?>
+          <th scope="col"><?php echo $key?></th>
+        <?php endforeach; ?>
+    </thead>
+    <tbody>
+      <?php foreach($hotels as $hotel): ?>
+      <tr>
+          <td scope="col">
+            <?php echo $hotel['name']?> 
+            <?php echo $hotel['description']?>
+            <?php echo $hotel['parking']?> 
+            <?php echo $hotel['vote']?>
+            <?php echo $hotel['distance_to_center']?>
+          </td>
+      </tr>
+      <?php endforeach; ?>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+
+</body>
+</html>
